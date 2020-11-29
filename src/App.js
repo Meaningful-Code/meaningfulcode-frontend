1ffe76bb491bfd4f30ee1a5b36f361c72e242d9e
Adrien Leravat <aleravat@witekio.com>
2020-11-28 18:57:53 -0800

    Share buttons: fix typo in the default message

--- src/App.js
+++ src/App.js
@@ -36,9 +36,9 @@ function Footer() {
     <footer>
       <div id='social'>
         <Header as='h3'>Support these projects: Share!</Header>
-        <Button color='twitter' href="https://twitter.com/intent/tweet?source=https%3A%2F%2Fmeaningfulcode.org&text=Pick%20a%20cause%20to%20support%2C%20find%20Open%20Source%20projects%20to%20contribute%20to%2C%20and%20write%2023MeaningfulCode%21%20%E2%9C%A8%20https%3A%2F%2Fmeaningfulcode.org%2F" target="_blank" title="Tweet"><Icon name='twitter' />Twitter</Button>
-        <Button color='linkedin' href='http://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fmeaningfulcode.org&title=MeaningfulCode%20-%20Impactful%20open-source&summary=Find%20impactful%20open-source%20projects%2C%20and%20make%20a%20difference&source=https%3A%2F%2Fmeaningfulcode.org' target="_blank" title="Share on LinkedIn"><Icon name='linkedin' />LinkedIn</Button>
-        <Button color='red' href="http://www.reddit.com/submit?url=https%3A%2F%2Fmeaningfulcode.org&title=MeaningfulCode%20-%20Impactful%20open-source" target="_blank" title="Submit to Reddit"><Icon name='reddit' />Reddit</Button>
+        <Button color='twitter' href='https://twitter.com/intent/tweet?source=https%3A%2F%2Fmeaningfulcode.org&text=Pick%20a%20cause%20to%20support%2C%20find%20Open%20Source%20projects%20to%20contribute%20to%2C%20and%20write%20%23MeaningfulCode%21%20%E2%9C%A8%20https%3A%2F%2Fmeaningfulcode.org%2F' target='_blank' title='Tweet'><Icon name='twitter' />Twitter</Button>
+        <Button color='linkedin' href='http://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fmeaningfulcode.org&title=MeaningfulCode%20-%20Impactful%20open-source&summary=Pick%20a%20cause%20to%20support%2C%20find%20Open%20Source%20projects%20to%20contribute%20to%2C%20and%20write%20%23MeaningfulCode%21%20%E2%9C%A8%20https%3A%2F%2Fmeaningfulcode.org%2F&source=https%3A%2F%2Fmeaningfulcode.org' target='_blank' title='Share on LinkedIn'><Icon name='linkedin' />LinkedIn</Button>
+        <Button color='red' href='http://www.reddit.com/submit?url=https%3A%2F%2Fmeaningfulcode.org&title=MeaningfulCode%20-%20Impactful%20open-source' target='_blank' title='Submit to Reddit'><Icon name='reddit' />Reddit</Button>
       </div>
     </footer>)
 }
