b079066fe8f70224917474ca24346b1d49d5d90f
Adrien Leravat <Pixep@users.noreply.github.com>
2021-10-09 11:27:57 -0700

    Move mock/test data to a dedicated file

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,87 +6,17 @@ import ProjectCard, { ProjectPlaceholder } from './main/ProjectCard';
 import ProjectsCategoryFilterMenu from './main/ProjectsCategoryFilterMenu';
 import ProjectsSortingMenu from './main/ProjectsSortingMenu';
 
+import getMockProjects from '../data/mock'
+
 import './main/Main.css';
 
 const sleep = (milliseconds) => {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
 
-function sampleProjects() {
-  return [
-    {
-      name: 'Project A',
-      owner: 'Octocat',
-      categories: ['humanitarian'],
-      languages: ['Python', 'HTML'],
-      stars: '120',
-      description: 'Description text that will likely span on multiple lines.',
-      url: 'https://github.com/project-a',
-      websiteUrl: 'https://websitea.com',
-      lastCommitTimestamp: '1607894587'
-    },
-    {
-      name: 'Project B',
-      owner: 'Octocow',
-      categories: ['health'],
-      languages: ['Python', 'HTML'],
-      stars: '13',
-      description: 'Description text that will likely span on multiple lines.',
-      url: 'https://github.com/project-b',
-      websiteUrl: 'https://websiteb.com',
-      lastCommitTimestamp: '1607824587'
-    },
-    {
-      name: 'Project C',
-      owner: 'Octocrow',
-      categories: ['health'],
-      languages: ['Python', 'HTML'],
-      stars: '1200',
-      description: 'Description text that will likely span on multiple lines.',
-      url: 'https://github.com/project-c',
-      websiteUrl: 'https://websitec.com',
-      lastCommitTimestamp: '1607124587'
-    },
-    {
-      name: 'Project D',
-      owner: 'Octocus',
-      categories: ['environment'],
-      languages: ['C++'],
-      stars: '320',
-      description:
-        'Description text that will likely span on multiple lines. Description text that will likely span on multiple lines. Description text that will likely span on multiple lines. Description text that will likely span on multiple lines. Description text that will likely span on multiple lines. Description text that will likely span on multiple lines. Description text that will likely span on multiple lines.',
-      url: 'https://github.com/project-d',
-      websiteUrl: 'https://websited.com',
-      lastCommitTimestamp: '1607824117'
-    },
-    {
-      name: 'Project E',
-      owner: 'Octocrate',
-      categories: ['environment'],
-      languages: ['C++', 'Bash'],
-      stars: '560',
-      description: 'Description text that will likely span on multiple lines.',
-      url: 'https://github.com/project-e',
-      websiteUrl: 'https://websited.com',
-      lastCommitTimestamp: '1507824117'
-    },
-    {
-      name: 'Project F',
-      owner: 'Octocanister-barista',
-      categories: ['society'],
-      languages: ['C', 'Go', 'Makefile', 'Dockerfile'],
-      stars: '4201',
-      description: 'Description text that will likely span on multiple lines.',
-      url: 'https://github.com/project-f',
-      websiteUrl: 'https://websitef.com',
-      lastCommitTimestamp: '1587824117'
-    }
-  ];
-}
-
 async function getProjects() {
   if (process.env.REACT_APP_FORCE_API === 'stub') {
-    return sampleProjects();
+    return getMockProjects();
   }
 
   let getProjectsApiUrl = '/api/projects';
