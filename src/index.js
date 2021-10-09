c53789dfe1f4fb91673cdcc24f67c055775078cc
Adrien Leravat <Pixep@users.noreply.github.com>
2021-10-09 11:15:38 -0700

    react: upgrade to 17.0.2

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactGA.initialize('UA-177770802-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
