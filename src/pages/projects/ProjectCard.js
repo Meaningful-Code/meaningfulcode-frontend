8d32457d09d8b1886e608bee244e0982c39c9fd0
Adrien Leravat <Pixep@users.noreply.github.com>
2022-07-24 15:37:24 -0700

    ProjectCard: Improve project placeholder design

--- src/pages/projects/ProjectCard.js
+++ src/pages/projects/ProjectCard.js
@@ -184,7 +184,7 @@ function ProjectCard(props) {
       data-owner={owner.toLowerCase()}
       data-desc={description.toLowerCase()}
     >
-      <Card>
+      <Card raised>
         <CardHeader
           component="h3"
           title={name}
@@ -261,19 +261,19 @@ ProjectCard.propTypes = {
 
 function ProjectPlaceholder() {
   return (
-    <Grid item xs={4}>
-      <Card raised className="project-item">
+    <Grid item xs={4} className="project-item">
+      <Card raised sx={{ padding: '1em' }}>
+        <Skeleton />
+        <Skeleton sx={{ marginBottom: '2em' }} />
+        <Skeleton width="75%" />
+        <Skeleton width="60%" />
+        <Skeleton width="70%" sx={{ marginBottom: '1em' }} />
         <Skeleton />
         <Skeleton />
         <Skeleton />
         <Skeleton />
         <Skeleton />
-        <Skeleton />
-        <Skeleton />
-        <Skeleton />
-        <Skeleton />
-        <Skeleton />
-        <Skeleton />
+        <Skeleton width="60%" sx={{ marginBottom: '2em' }} />
       </Card>
     </Grid>
   );
