23ffb5f73620e2b3e668bc74bf7a3ffb20a3fd73
Adrien Leravat <Pixep@users.noreply.github.com>
2022-07-30 18:15:08 -0700

    Migrate main index and App component to TS

--- src/index.tsx
+++ src/index.tsx
@@ -7,5 +7,5 @@ import App from './App';
 ReactGA.initialize('UA-177770802-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root')!);
 root.render(<App />);
