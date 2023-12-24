9f0b2de17dffbac8f1fe764001711f43b4d603c3
Adrien Leravat <Pixep@users.noreply.github.com>
2023-12-23 17:28:56 -0800

    Update sorting buttons color

--- src/theme/createDefaultTheme.tsx
+++ src/theme/createDefaultTheme.tsx
@@ -1,11 +1,11 @@
 import { Theme, createTheme } from '@mui/material/styles';
 
 export default function createDefaultTheme(): Theme {
-    return createTheme({
+  return createTheme({
     palette: {
       primary: { main: '#3c9d76', contrastText: '#fff' },
       secondary: { main: '#1f647d', contrastText: '#fff' },
-      neutral: { main: '#9b9c98', contrastText: '#000' },
+      neutral: { main: '#0b7c78', contrastText: '#000' },
     },
     typography: {
       h1: { fontSize: '2em' },
