929c6b7b96f6f78356a3c5c02914034e0405226e
Adrien Leravat <aleravat@witekio.com>
2024-01-02 15:09:27 -0800

    Update mobile menu for dark mode

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -5,6 +5,7 @@ import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import { useTheme } from '@mui/material/styles';
 
 import HomeIcon from '@mui/icons-material/Home';
 import InfoIcon from '@mui/icons-material/Info';
@@ -14,14 +15,17 @@ import RocketIcon from '@mui/icons-material/Rocket';
 import { Link as RouterLink } from 'react-router-dom';
 
 export default function MobileMenu(): JSX.Element {
+  const theme = useTheme();
+  const isLightMode = theme.palette.mode === 'light';
+  const color = isLightMode ? 'var(--black)' : 'var(--white)';
   return (
-    <List>
+    <List className="mobile-menu">
       <ListItem component={RouterLink} to="/submit-project">
         <ListItemButton>
           <ListItemIcon>
             <AddIcon />
           </ListItemIcon>
-          <ListItemText primary="Submit project" sx={{color: 'var(--black)'}}/>
+          <ListItemText primary="Submit project" sx={{ color: color }} />
         </ListItemButton>
       </ListItem>
       <Divider />
@@ -30,7 +34,7 @@ export default function MobileMenu(): JSX.Element {
           <ListItemIcon>
             <HomeIcon />
           </ListItemIcon>
-          <ListItemText primary="Home" sx={{color: 'var(--black)'}} />
+          <ListItemText primary="Home" sx={{ color: color }} />
         </ListItemButton>
       </ListItem>
       <ListItem component={RouterLink} to="/get-started">
@@ -38,7 +42,7 @@ export default function MobileMenu(): JSX.Element {
           <ListItemIcon>
             <RocketIcon />
           </ListItemIcon>
-          <ListItemText primary="Getting started"  sx={{color: 'var(--black)'}}/>
+          <ListItemText primary="Getting started" sx={{ color: color }} />
         </ListItemButton>
       </ListItem>
       <ListItem component={RouterLink} to="/about">
@@ -46,7 +50,7 @@ export default function MobileMenu(): JSX.Element {
           <ListItemIcon>
             <InfoIcon />
           </ListItemIcon>
-          <ListItemText primary="About"  sx={{color: 'var(--black)'}}/>
+          <ListItemText primary="About" sx={{ color: color }} />
         </ListItemButton>
       </ListItem>
     </List>
