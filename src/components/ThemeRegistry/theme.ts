43f854499a2f0f77984f8023b617218fb7e51ea8
Adrien Leravat <Pixep@users.noreply.github.com>
2024-01-08 21:47:21 -0800

    Integrate original theme in light mode

--- src/components/ThemeRegistry/theme.ts
+++ src/components/ThemeRegistry/theme.ts
@@ -1,29 +1,52 @@
-import { Roboto } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
 
-const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-});
-
+// FIXME: Restore dark mode
+const lightMode = true;
 const theme = createTheme({
   palette: {
-    mode: 'light',
+    mode: lightMode ? 'light' : 'dark',
+    primary: {
+      main: lightMode ? '#3c9d76' : '#54a484',
+      contrastText: lightMode ? '#fff' : '#000',
+    },
+    secondary: { main: lightMode ? '#1f647d' : '#62a7d0', contrastText: '#fff' },
+    neutral: {
+      main: lightMode ? '#0b7c78' : '#d0d0d0',
+      contrastText: lightMode ? '#000' : '#000',
+    },
+    cat_all: {
+      main: lightMode ? '#1f647d' : '#287693',
+      contrastText: lightMode ? '#000' : '#000',
+    },
+    cat_environment: {
+      main: lightMode ? '#3c9d76' : '#3c9d76',
+      contrastText: lightMode ? '#000' : '#fff',
+    },
+    cat_humanitarian: {
+      main: lightMode ? '#cc8562' : '#cc8562',
+      contrastText: lightMode ? '#000' : '#fff',
+    },
+    cat_accessibility: {
+      main: lightMode ? '#1f647d' : '#287693',
+      contrastText: lightMode ? '#000' : '#fff',
+    },
+    cat_society: {
+      main: lightMode ? '#1f647d' : '#287693',
+      contrastText: lightMode ? '#000' : '#fff',
+    },
+    cat_health: {
+      main: lightMode ? '#cc8562' : '#cc8562',
+      contrastText: lightMode ? '#000' : '#fff',
+    },
+    cat_education: {
+      main: lightMode ? '#3c9d76' : '#3c9d76',
+      contrastText: lightMode ? '#000' : '#fff',
+    },
   },
   typography: {
-    fontFamily: roboto.style.fontFamily,
-  },
-  components: {
-    MuiAlert: {
-      styleOverrides: {
-        root: ({ ownerState }) => ({
-          ...(ownerState.severity === 'info' && {
-            backgroundColor: '#60a5fa',
-          }),
-        }),
-      },
-    },
+    h1: { fontSize: '2em' },
+    h2: { fontSize: '1.7em' },
+    h3: { fontSize: '1.5em' },
   },
 });
 
