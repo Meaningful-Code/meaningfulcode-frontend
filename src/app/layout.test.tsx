d0cebbeff2f89a830ebaf4fae8f13615c52611a0
Adrien Leravat <Pixep@users.noreply.github.com>
2024-01-09 20:48:10 -0800

    tests: fix layout/App test

--- src/app/layout.test.tsx
+++ src/app/layout.test.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-import RootLayout from './layout';
+import { App } from './layout';
 
 describe('RootLayout', () => {
   it('renders', async () => {
-    render(<RootLayout>Hello</RootLayout>);
+    render(<App>Hello</App>);
   });
 });
