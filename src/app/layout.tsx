9c193881448100ef7fa3328e11331b7b3c625d1d
Adrien Leravat <Pixep@users.noreply.github.com>
2024-01-13 15:56:53 -0800

    Restore minimal GA page events

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
+import dynamic from 'next/dynamic';
+import Script from 'next/script';
 import Container from '@mui/material/Container';
 import ThemeRegistry from '@/components/ThemeRegistry/ThemeRegistry';
+
+import useMinimalGaTracker from '../components/GaPageEvent';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -60,10 +64,30 @@ export function App({ children }: { children: React.ReactNode }) {
   );
 }
 
+const GaPageEvent = dynamic(() => import('@/components/GaPageEvent'), {
+  ssr: false,
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-C9SQS63TJQ" />
+        <Script id="gtag">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+              dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', 'G-C9SQS63TJQ', {
+              anonymize_ip: true,
+              client_storage: 'none',
+              ad_storage: 'denied',
+              analytics_storage: 'denied',
+            });`}
+        </Script>
+        <GaPageEvent />
         <App>{children}</App>
       </body>
     </html>
---
+++ src/components/GaPageEvent.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+
+const GaPageEvent = () => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (!window.gtag) {
+      return;
+    }
+    window.gtag('config', 'G-C9SQS63TJQ', {
+      page_path: pathname + searchParams,
+    });
+  }, [pathname, searchParams]);
+  return <></>;
+};
+
+export default GaPageEvent;
