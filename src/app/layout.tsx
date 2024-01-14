bda35f02576980840bf73275bdc51f05b1adbfc6
Adrien Leravat <Pixep@users.noreply.github.com>
2024-01-13 16:32:10 -0800

    Restore basic dark mode support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,40 +18,6 @@ export const metadata = {
 };
 
 export function App({ children }: { children: React.ReactNode }) {
-  // const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)');
-  // const theme = useMemo(() => createDefaultTheme(prefersLightMode), [prefersLightMode]);
-  // useEffect(() => {
-  //   const root = document.documentElement;
-  //   root.style.setProperty('--gray', '#888');
-  //   root.style.setProperty('--white', '#fff');
-  //   root.style.setProperty('--black', '#000');
-
-  //   /* @ts-ignore: color type not properly recognized */
-  //   root.style.setProperty('--cat-all-color', theme.palette.cat_all.main);
-  //   /* @ts-ignore: color type not properly recognized */
-  //   root.style.setProperty('--cat-health-color', theme.palette.cat_health.main);
-  //   /* @ts-ignore: color type not properly recognized */
-  //   root.style.setProperty('--cat-education-color', theme.palette.cat_education.main);
-  //   root.style.setProperty(
-  //     '--cat-environment-color',
-  //     /* @ts-ignore: color type not properly recognized */
-  //     theme.palette.cat_environment.main
-  //   );
-  //   /* @ts-ignore: color type not properly recognized */
-  //   root.style.setProperty('--cat-society-color', theme.palette.cat_society.main);
-  //   root.style.setProperty(
-  //     '--cat-humanitarian-color',
-  //     /* @ts-ignore: color type not properly recognized */
-  //     theme.palette.cat_humanitarian.main
-  //   );
-  //   root.style.setProperty(
-  //     '--cat-accessibility-color',
-  //     /* @ts-ignore: color type not properly recognized */
-  //     theme.palette.cat_accessibility.main
-  //   );
-  //   document.body.style.backgroundColor = theme.palette.background.default;
-  // }, [theme.palette]);
-
   return (
     <ThemeRegistry>
       <div id="root">
--- src/components/ThemeRegistry/ThemeRegistry.tsx
+++ src/components/ThemeRegistry/ThemeRegistry.tsx
@@ -1,14 +1,50 @@
 'use client';
+
 import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import NextAppDirEmotionCacheProvider from './EmotionCache';
-import theme from './theme';
+import createCustomTheme from './theme';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 export default function ThemeRegistry({ children }: { children: React.ReactNode }) {
+  const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)');
+  // const theme = useMemo(() => createDefaultTheme(prefersLightMode), [prefersLightMode]);
+  // useEffect(() => {
+  //   const root = document.documentElement;
+  //   root.style.setProperty('--gray', '#888');
+  //   root.style.setProperty('--white', '#fff');
+  //   root.style.setProperty('--black', '#000');
+
+  //   /* @ts-ignore: color type not properly recognized */
+  //   root.style.setProperty('--cat-all-color', theme.palette.cat_all.main);
+  //   /* @ts-ignore: color type not properly recognized */
+  //   root.style.setProperty('--cat-health-color', theme.palette.cat_health.main);
+  //   /* @ts-ignore: color type not properly recognized */
+  //   root.style.setProperty('--cat-education-color', theme.palette.cat_education.main);
+  //   root.style.setProperty(
+  //     '--cat-environment-color',
+  //     /* @ts-ignore: color type not properly recognized */
+  //     theme.palette.cat_environment.main
+  //   );
+  //   /* @ts-ignore: color type not properly recognized */
+  //   root.style.setProperty('--cat-society-color', theme.palette.cat_society.main);
+  //   root.style.setProperty(
+  //     '--cat-humanitarian-color',
+  //     /* @ts-ignore: color type not properly recognized */
+  //     theme.palette.cat_humanitarian.main
+  //   );
+  //   root.style.setProperty(
+  //     '--cat-accessibility-color',
+  //     /* @ts-ignore: color type not properly recognized */
+  //     theme.palette.cat_accessibility.main
+  //   );
+  //   document.body.style.backgroundColor = theme.palette.background.default;
+  // }, [theme.palette]);
+
   return (
     <NextAppDirEmotionCacheProvider options={{ key: 'mui' }}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={createCustomTheme(prefersLightMode)}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         {children}
