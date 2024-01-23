5c74ff478bdb06ed7cba28fa5f93a2fe4a4e1ecb
Adrien Leravat <Pixep@users.noreply.github.com>
2024-01-22 19:19:34 -0800

    package.json, settings: refresh based on src tree

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+};
 
 module.exports = nextConfig;
