3415bb3bf3c6be068075b8d9fd7a3b4d5dd23035
Adrien Leravat <Pixep@users.noreply.github.com>
2024-06-19 20:35:10 -0700

    find-project: improve header description

--- src/app/find-project/page.tsx
+++ src/app/find-project/page.tsx
@@ -2,7 +2,10 @@
 
 import React, { Suspense } from 'react';
 import { Metadata } from 'next';
+import Link from 'next/link';
+
 import Typography from '@mui/material/Typography';
+import Alert from '@mui/material/Alert';
 
 import getHost from '@/utils/getHost';
 
@@ -15,9 +18,20 @@ export const metadata: Metadata = {
 export default async function FindProject() {
   return (
     <>
-      <Typography variant="h1">Find my project!</Typography>
-      <p>Ask away, and let the AI agent recommend projects for you to contribute to.</p>
-      <Typography variant="h2">I am looking for ...</Typography>
+      <Typography variant="h1">Natural language project search</Typography>
+      <p>
+        This page lets you search for projects using natural language from the same list
+        of projects. For a more targeted search, please continue using the{' '}
+        <Link href="/">main search page</Link> and its category, language, and search
+        filters.
+      </p>
+      <Alert severity="info">
+        This feature is experimental, and the quality of results may vary. We would love
+        to hear your <Link href="/about">feedback</Link> if you have time!
+      </Alert>
+      <br />
+      <br />
+      <Typography variant="h2">What are you looking for?</Typography>
       <br />
       <Suspense>
         <FindProjectForm host={getHost()} />
